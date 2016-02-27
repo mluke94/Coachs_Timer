a83e6063a521b2df64f8614b0a4205b63d51cdd7
Samuel Rubenking <Samuel Rubenking>
2016-02-27 01:52:38 -0500

    updated Timer

--- app/src/main/java/hacks/coachs_timer/Timer.java
+++ app/src/main/java/hacks/coachs_timer/Timer.java
@@ -1,8 +1,12 @@
 package hacks.coachs_timer;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Sam on 2/27/2016.
@@ -13,11 +17,49 @@ public class Timer implements Parcelable {
     private short splitNum; // current split number
     private boolean started; // is the timer running
     private boolean stopSplit; // does it split when stopped
-    private long timeInMilliseconds = 0L; // Initialize and set to zero the time in milliseconds
-    private long timeSwapBuff = 0L; // buffer time when paused
-    private long updatedTime = 0L; // updated time for timer
-    private long lastTime = 0L; // total time at last split
-    private long currentTime = 0L; // total time at current split
-    private long startTime = 0L; // time at start
+    private long timeInMilliseconds; // Initialize and set to zero the time in milliseconds
+    private long timeSwapBuff; // buffer time when paused
+    private long updatedTime; // updated time for timer
+    private long lastTime; // total time at last split
+    private long currentTime ; // total time at current split
+    private long startTime; // time at start
     private Time time, split; // object of total time and current split
+
+    public Timer(){
+        splits = new ArrayList<Split>();
+        splitNum = 1;
+        started = false;
+        stopSplit = false;
+        timeInMilliseconds = 0L;
+        timeSwapBuff = 0L;
+        updatedTime = 0L;
+        lastTime = 0L;
+        currentTime = 0L;
+        startTime = 0L;
+        time = new Time(0L);
+        split = new Time(0L);
+    }
+    // Parcelling part
+    public Timer(Parcel in) {
+        int cnt = in.readInt();
+        long[] data = new long[cnt];
+        in.readLongArray(data);
+
+    }
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        ArrayList<Long> tempArray = new ArrayList<Long>();
+        List<Long> newList = Arrays.asList(new Long[] {(long) splitNum,timeInMilliseconds,
+                timeSwapBuff,updatedTime,lastTime,currentTime,startTime,time.getTime(),
+                split.getTime()});
+        tempArray.addAll(newList);
+        for (Split s:splits) {
+            List<Long> splitList = Arrays.asList(s.getLongArray());
+            tempArray.addAll(splitList);
+        }
+    }
 }
