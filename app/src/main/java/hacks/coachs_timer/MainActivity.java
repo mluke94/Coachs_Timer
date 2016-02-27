0f2d62eb9d64828985ce596815069c3915c2f5d0
Samuel Rubenking <Samuel Rubenking>
2016-02-27 08:17:58 -0500

    finished Timer class

--- app/src/main/java/hacks/coachs_timer/MainActivity.java
+++ app/src/main/java/hacks/coachs_timer/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        timeDisplay = (TextField)findViewById(R.id.);
+
 
         setContentView(R.layout.activity_main);
     }
