62578e0715b7c174075aa45f69fe121bc9d4a079
Samuel Rubenking <Samuel Rubenking>
2016-02-28 05:14:14 -0500

    no update

--- app/src/main/java/hacks/coachs_timer/GroupTimerFragment.java
+++ app/src/main/java/hacks/coachs_timer/GroupTimerFragment.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by LukeM on 2/27/2016.
